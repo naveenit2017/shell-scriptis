237c1e08ff62b6c3b51e4b7ec8aa63f39aea6e8b
naveenit2017 <114174494+naveenit2017@users.noreply.github.com>
2024-07-16 07:50:18 +0530

    Create log-roation.sh

---
+++ log-roation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "This script will help us to delete 30 days older files"
+path=$1
+find $path -mtime +30 -delete $1
+if [ $? -eq 0 ];then
+        echo "The files are deleted successfully"
+else
+        echo "There is a problem please check the logic"
+fi
